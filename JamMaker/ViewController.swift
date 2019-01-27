822e00ad6f62768b2749235cf06c06fce75f1bbd
Hsuen-Ju Li <lhj@hsuen-jude-macbook-pro.local>
2019-01-27 17:55:58 +0800

    Initial Commit

---
+++ JamMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JamMaker
+//
+//  Created by Hsuen-Ju Li on 2019/1/27.
+//  Copyright © 2019 Hsuen-Ju Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
